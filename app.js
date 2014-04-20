4048e8821cbfc283201c215c399d04b363d44a58
Alf <alfg@users.noreply.github.com>
2014-04-19 18:36:28 -0700

    Fix less-middleware usage error.
    http://stackoverflow.com/questions/22285659/first-nodejs-app-it-is-saying-to-update-your-less-middleware-usage

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.configure(function(){
   app.use(express.cookieParser(config.web.sessionkey));
   app.use(express.cookieSession());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(require('less-middleware')(path.join(__dirname, 'public')));
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(routes.checkAuth);
 });
